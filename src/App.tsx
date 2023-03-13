8d88c977b33e0495099a4284a7bfa2a839e83a9f
Serhii Shabala <sergei181989@gmail.com>
2023-03-13 13:38:45 +0200

    :sparkles: add: Login component & rename folder (ls_04)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import { Home } from './components';
+import { Login } from './components/Auth/Login';
 
 export const App = () => {
   return (
     <div className='App'>
-      <Home />
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='login'
+          element={<Login />}
+        />
+      </Routes>
     </div>
   );
 };
